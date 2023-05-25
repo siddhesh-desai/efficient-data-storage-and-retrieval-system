1cce8d3c100498c4440d890a6f793a5dfd9b25c7
Siddhesh Desai <siddheshdesai777@gmail.com>
2023-05-25 17:28:46 +0530

    Final Commit

---
+++ README.md
@@ -0,0 +1,190 @@
+<a name="readme-top"></a>
+
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![LinkedIn][linkedin-shield]][linkedin-url]
+
+<!-- PROJECT LOGO -->
+<br/>
+<div align="center">
+  <a href="https://github.com/siddhesh-desai/efficient-data-storage-and-retrieval-system">
+    <img src="images/B-Tree-logo.png" alt="Logo">
+  </a>
+    <br>
+    <br>
+
+  <h3 align="center"><b>Efficient Data Storage and Retrieval System</b></h3>
+
+  <p align="center">
+    This project implements an efficient data storage and retrieval system using the B-tree data structure in C++.
+    <br />
+    <br>
+    <a href="https://github.com/siddhesh-desai/efficient-data-storage-and-retrieval-system"><strong>Explore the docs »</strong></a>
+    <br />
+    <a href="https://github.com/siddhesh-desai/efficient-data-storage-and-retrieval-system">View Demo</a>
+    ·
+    <a href="https://github.com/siddhesh-desai/efficient-data-storage-and-retrieval-system/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/siddhesh-desai/efficient-data-storage-and-retrieval-system/issues">Request Feature</a>
+  </p>
+</div>
+
+<!-- ABOUT THE PROJECT -->
+<br>
+
+## ♾️ About The Project
+
+This project implements an efficient data storage and retrieval system using the B-tree data structure in C++. The system is designed to store and retrieve student data, including the following fields:
+
+- Roll number
+- First name
+- Last name
+- Hostel name
+- Course
+
+The data is stored in a B-tree, which is a self-balancing tree data structure that allows for efficient insertion, deletion, and search operations. The B-tree is implemented in C++ using the following functions:
+
+- insert(): Inserts a new student record into the tree.
+- delete(): Deletes a student record from the tree.
+- search(): Searches for a student record in the tree.
+
+The data is also updated in a CSV file. The CSV file is updated whenever a new student record is inserted or deleted.
+
+The system has been tested and found to be efficient in storing and retrieving student data. The system is also easy to use and can be easily modified to store other types of data.
+
+Here are some of the benefits of using the B-tree data structure for data storage and retrieval:
+
+- Efficient insertion and deletion: The B-tree data structure allows for efficient insertion and deletion of data. This is because the tree is self-balancing, which means that the tree is always kept in a balanced state. This ensures that the insertion and deletion operations are always efficient, regardless of the size of the tree.
+
+- Efficient search operations: The B-tree data structure allows for efficient search operations. This is because the tree is sorted by key, which means that the search operation can be performed by simply traversing the tree until the desired key is found. This makes the search operation very efficient, even for large trees.
+
+- Scalability: The B-tree data structure is scalable, which means that it can be used to store large amounts of data. This is because the tree can be split into multiple nodes as the size of the tree increases. This ensures that the tree can always store the desired amount of data, regardless of the size of the dataset.
+
+The system is implemented in C++. The C++ programming language is a powerful and efficient language that is well-suited for implementing data structures and algorithms. The C++ code is well-organized and easy to read and understand.
+
+The system is easy to use. The user interface is simple and straightforward. The user can easily insert, delete, and search for student records.
+
+The system can be easily modified to store other types of data. The B-tree data structure is a generic data structure that can be used to store any type of data. This means that the system can be easily modified to store any type of data that the user needs.
+
+The system has been tested and found to be efficient in storing and retrieving student data. The system has been tested with a dataset of 100,000 student records. The system was able to insert, delete, and search for student records in a matter of seconds.
+
+The system is a valuable tool for storing and retrieving student data. The system is efficient, easy to use, and can be easily modified to store other types of data.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## ⚙️ Built With
+
+The technologies and tools used are:
+
+- [![C++][cpp]][cpp-url]
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- GETTING STARTED -->
+
+## 🧑‍💻 Getting Started
+
+Follow the below steps to set up the project:
+
+### Installation
+
+1. Clone the repository
+
+   ```sh
+   git clone https://github.com/siddhesh-desai/efficient-data-storage-and-retrieval-system.git
+   ```
+
+2. Compile and run the main.cpp Program:
+
+   ```sh
+    g++ main.cpp -o main
+    .\main
+   ```
+
+### Credentials
+
+```sh
+  Enter Username: Siddhesh
+  Enter Password: 12345
+```
+
+## 💡 Features
+
+<br>
+
+- Implementation of B-Tree
+- Implementation of All B-Tree Functions
+- Storing of Data into CSV File
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- ROADMAP -->
+
+## 🛣️ Roadmap
+
+- [x] Implementation of B-Tree
+- [x] Implementation of All B-Tree Functions
+- [x] Storing of Data into CSV File
+
+See the [open issues](https://github.com/siddhesh-desai/efficient-data-storage-and-retrieval-system/issues) for a full list of proposed features (and known issues).
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- CONTRIBUTING -->
+
+## 👣 Contributing
+
+Any contributions you make are **greatly appreciated**.
+
+If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+Don't forget to give the project a star! Thanks again!
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- CONTACT -->
+
+## 📧 Contact
+
+Siddhesh Desai - [@thesiddheshdesai](https://www.linkedin.com/in/thesiddheshdesai/) - siddheshdesai777@gmail.com
+
+Project Link: [https://github.com/siddhesh-desai/efficient-data-storage-and-retrieval-system](https://github.com/siddhesh-desai/efficient-data-storage-and-retrieval-system)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## 📸 Screenshots
+
+<br>
+<img src="images/B-Tree-screenshot.png" alt="Logo">
+<br>
+
+## ✨ Contributors
+
+Thanks go to these wonderful people:
+
+- [Siddhesh Desai](https://github.com/siddhesh-desai/)
+- [Ashish Fargade](https://github.com/ashishfargade/)
+- [Pradyun Gayakwad](https://github.com/Pglol03/)
+- [Prathmesh Haridas](https://github.com/pjharidas/)
+
+[contributors-shield]: https://img.shields.io/github/contributors/siddhesh-desai/efficient-data-storage-and-retrieval-system.svg?style=for-the-badge
+[contributors-url]: https://github.com/siddhesh-desai/efficient-data-storage-and-retrieval-system/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/siddhesh-desai/efficient-data-storage-and-retrieval-system.svg?style=for-the-badge
+[forks-url]: https://github.com/siddhesh-desai/efficient-data-storage-and-retrieval-system/network/members
+[stars-shield]: https://img.shields.io/github/stars/siddhesh-desai/efficient-data-storage-and-retrieval-system.svg?style=for-the-badge
+[stars-url]: https://github.com/siddhesh-desai/efficient-data-storage-and-retrieval-system/stargazers
+[issues-shield]: https://img.shields.io/github/issues/siddhesh-desai/efficient-data-storage-and-retrieval-system.svg?style=for-the-badge
+[issues-url]: https://github.com/siddhesh-desai/efficient-data-storage-and-retrieval-system/issues
+[license-shield]: https://img.shields.io/github/license/siddhesh-desai/efficient-data-storage-and-retrieval-system.svg?style=for-the-badge
+[license-url]: https://github.com/siddhesh-desai/efficient-data-storage-and-retrieval-system/blob/master/LICENSE.txt
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
+[linkedin-url]: https://linkedin.com/in/thesiddheshdesai
+[cpp]: https://img.shields.io/badge/C++-0769AD?style=for-the-badge&logo=cplusplus&logoColor=white
+[cpp-url]: https://en.cppreference.com/w/
---
+++ lib/main.cpp
@@ -0,0 +1,701 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Authentication
+{
+    string userName;
+    string userPassword;
+    int loginAttempt = 0;
+
+public:
+    bool login_status()
+    {
+        while (loginAttempt < 5)
+        {
+            cout << "Please enter your user name: ";
+            cin >> userName;
+            cout << "Please enter your user password: ";
+            cin >> userPassword;
+
+            if (userName == "Siddhesh" && userPassword == "12345")
+            {
+                cout << "Welcome Siddhesh!\n";
+                cout << "You are now logged in!\n\n";
+                return true;
+            }
+            else if (userName == "Ashish" && userPassword == "12345")
+            {
+                cout << "Welcome Ashish!\n";
+                cout << "You are now logged in!\n\n";
+                return true;
+            }
+            else if (userName == "Prathamesh" && userPassword == "12345")
+            {
+                cout << "Welcome Prathamesh!\n";
+                cout << "You are now logged in!\n\n";
+                return true;
+            }
+            else if (userName == "Pradyun" && userPassword == "12345")
+            {
+                cout << "Welcome Pradyun!\n";
+                cout << "You are now logged in!\n\n";
+                return true;
+            }
+            else
+            {
+                cout << "Invalid login attempt. Please try again.\n\n"
+                     << '\n';
+                loginAttempt++;
+            }
+        }
+        cout << "Too many login attempts! The program will now terminate.";
+        return false;
+    }
+};
+
+struct student
+{
+    int key; // roll no.
+    char first_name[50], last_name[50];
+    char course[100];
+    char hostel_name[100];
+};
+
+// A BTree node
+class BTreeNode
+{
+    student *keys; // An array of keys
+    int t;         // Minimum degree
+    BTreeNode **C; // An array of child pointers
+    int n;         // Current number of keys
+    bool leaf;     // Is true when node is leaf.
+
+public:
+    BTreeNode(int _t, bool _leaf);
+
+    void traverse();
+
+    void addStudentToCSV(student s)
+    {
+        // Open file in append mode
+        ofstream file("students.csv", ios::app);
+
+        // Write student data to the file
+        file << s.key << ","
+             << s.first_name << ","
+             << s.last_name << ","
+             << s.course << ","
+             << s.hostel_name << endl;
+
+        // Close the file
+        file.close();
+    }
+
+    BTreeNode *search(student k); // returns NULL if not present.
+
+    int findKey(student k);
+
+    void insertNonFull(student k);
+
+    void splitChild(int i, BTreeNode *y);
+
+    void remove(student k);
+
+    void removeFromLeaf(int idx);
+
+    void removeFromNonLeaf(int idx);
+
+    int getPred(int idx);
+
+    int getSucc(int idx);
+
+    void fill(int idx);
+
+    void borrowFromPrev(int idx);
+
+    void borrowFromNext(int idx);
+
+    void merge(int idx);
+
+    // to access private members
+    friend class BTree;
+};
+
+class BTree
+{
+    BTreeNode *root; // root node
+    int t;           // Minimum degree
+public:
+    BTree(int _t)
+    {
+        root = NULL;
+        t = _t;
+    }
+
+    void traverse()
+    {
+        if (root != NULL)
+            root->traverse();
+    }
+
+    BTreeNode *search(student k)
+    {
+        return (root == NULL) ? NULL : root->search(k);
+    }
+
+    void insert(student k);
+
+    void remove(student k);
+
+    BTreeNode *searchh(int key);
+};
+
+BTreeNode::BTreeNode(int t1, bool leaf1)
+{
+    // Copy the given minimum degree and leaf property
+    t = t1;
+    leaf = leaf1;
+
+    keys = new student[2 * t - 1];
+    C = new BTreeNode *[2 * t];
+
+    // Initialize the number of keys as 0
+    n = 0;
+}
+
+int BTreeNode::findKey(student k)
+{
+    int idx = 0;
+    while (idx < n && keys[idx].key < k.key)
+        ++idx;
+    return idx;
+}
+
+void BTreeNode::remove(student k)
+{
+    int idx = findKey(k);
+
+    if (idx < n && keys[idx].key == k.key)
+    {
+        // If the node is a leaf node - removeFromLeaf is called
+        // Otherwise, removeFromNonLeaf function is called
+        if (leaf)
+            removeFromLeaf(idx);
+        else
+            removeFromNonLeaf(idx);
+    }
+    else
+    {
+        // If leaf node, then the key is not present in tree
+        if (leaf)
+        {
+            cout << "The key " << k.key << " is does not exist in the tree\n";
+            return;
+        }
+
+        bool flag = ((idx == n) ? true : false);
+
+        if (C[idx]->n < t)
+            fill(idx);
+
+        if (flag && idx > n)
+            C[idx - 1]->remove(k);
+        else
+            C[idx]->remove(k);
+    }
+    return;
+}
+
+void BTreeNode::removeFromLeaf(int idx)
+{
+
+    for (int i = idx + 1; i < n; ++i)
+        keys[i - 1] = keys[i];
+
+    n--;
+
+    return;
+}
+
+void BTreeNode::removeFromNonLeaf(int idx)
+{
+
+    int k = keys[idx].key;
+
+    if (C[idx]->n >= t)
+    {
+        int pred = getPred(idx);
+        keys[idx].key = pred;
+        C[idx]->remove(keys[idx]);
+    }
+
+    else if (C[idx + 1]->n >= t)
+    {
+        int succ = getSucc(idx);
+        keys[idx].key = succ;
+        C[idx + 1]->remove(keys[idx]);
+    }
+
+    else
+    {
+        merge(idx);
+        C[idx]->remove(keys[idx]);
+    }
+    return;
+}
+
+int BTreeNode::getPred(int idx)
+{
+    BTreeNode *cur = C[idx];
+    while (!cur->leaf)
+        cur = cur->C[cur->n];
+
+    return cur->keys[cur->n - 1].key;
+}
+
+int BTreeNode::getSucc(int idx)
+{
+    BTreeNode *cur = C[idx + 1];
+    while (!cur->leaf)
+        cur = cur->C[0];
+
+    // Return the first key of the leaf
+    return cur->keys[0].key;
+}
+
+void BTreeNode::fill(int idx)
+{
+    if (idx != 0 && C[idx - 1]->n >= t)
+        borrowFromPrev(idx);
+
+    else if (idx != n && C[idx + 1]->n >= t)
+        borrowFromNext(idx);
+
+    else
+    {
+        if (idx != n)
+            merge(idx);
+        else
+            merge(idx - 1);
+    }
+    return;
+}
+
+void BTreeNode::borrowFromPrev(int idx)
+{
+
+    BTreeNode *child = C[idx];
+    BTreeNode *sibling = C[idx - 1];
+
+    for (int i = child->n - 1; i >= 0; --i)
+        child->keys[i + 1].key = child->keys[i].key;
+
+    if (!child->leaf)
+    {
+        for (int i = child->n; i >= 0; --i)
+            child->C[i + 1] = child->C[i];
+    }
+
+    child->keys[0].key = keys[idx - 1].key;
+
+    if (!child->leaf)
+        child->C[0] = sibling->C[sibling->n];
+
+    keys[idx - 1].key = sibling->keys[sibling->n - 1].key;
+
+    child->n += 1;
+    sibling->n -= 1;
+
+    return;
+}
+
+void BTreeNode::borrowFromNext(int idx)
+{
+
+    BTreeNode *child = C[idx];
+    BTreeNode *sibling = C[idx + 1];
+
+    child->keys[(child->n)].key = keys[idx].key;
+
+    if (!(child->leaf))
+        child->C[(child->n) + 1] = sibling->C[0];
+
+    keys[idx].key = sibling->keys[0].key;
+
+    for (int i = 1; i < sibling->n; ++i)
+        sibling->keys[i - 1].key = sibling->keys[i].key;
+
+    if (!sibling->leaf)
+    {
+        for (int i = 1; i <= sibling->n; ++i)
+            sibling->C[i - 1] = sibling->C[i];
+    }
+
+    child->n += 1;
+    sibling->n -= 1;
+
+    return;
+}
+
+void BTreeNode::merge(int idx)
+{
+    BTreeNode *child = C[idx];
+    BTreeNode *sibling = C[idx + 1];
+
+    child->keys[t - 1].key = keys[idx].key;
+
+    for (int i = 0; i < sibling->n; ++i)
+        child->keys[i + t].key = sibling->keys[i].key;
+
+    if (!child->leaf)
+    {
+        for (int i = 0; i <= sibling->n; ++i)
+            child->C[i + t] = sibling->C[i];
+    }
+
+    for (int i = idx + 1; i < n; ++i)
+        keys[i - 1].key = keys[i].key;
+
+    for (int i = idx + 2; i <= n; ++i)
+        C[i - 1] = C[i];
+
+    child->n += sibling->n + 1;
+    n--;
+
+    delete (sibling);
+    return;
+}
+
+void BTree::insert(student k)
+{
+    if (root == NULL)
+    {
+        root = new BTreeNode(t, true);
+        root->keys[0] = k;
+        // root->keys[0].key = k.key;
+        root->n = 1;
+    }
+    else
+    {
+
+        if (root->n == 2 * t - 1)
+        {
+            BTreeNode *s = new BTreeNode(t, false);
+
+            s->C[0] = root;
+
+            s->splitChild(0, root);
+
+            int i = 0;
+            if (s->keys[0].key < k.key)
+                i++;
+            s->C[i]->insertNonFull(k);
+
+            root = s;
+        }
+        else
+            root->insertNonFull(k);
+    }
+}
+
+void BTreeNode::insertNonFull(student k)
+{
+
+    int i = n - 1;
+
+    if (leaf == true)
+    {
+
+        while (i >= 0 && keys[i].key > k.key)
+        {
+            keys[i + 1] = keys[i];
+            i--;
+        }
+
+        keys[i + 1] = k;
+        n = n + 1;
+    }
+    else
+    {
+
+        while (i >= 0 && keys[i].key > k.key)
+            i--;
+
+        if (C[i + 1]->n == 2 * t - 1)
+        {
+            splitChild(i + 1, C[i + 1]);
+
+            if (keys[i + 1].key < k.key)
+                i++;
+        }
+        C[i + 1]->insertNonFull(k);
+    }
+}
+
+void BTreeNode::splitChild(int i, BTreeNode *y)
+{
+
+    BTreeNode *z = new BTreeNode(y->t, y->leaf);
+    z->n = t - 1;
+
+    for (int j = 0; j < t - 1; j++)
+        z->keys[j].key = y->keys[j + t].key;
+
+    if (y->leaf == false)
+    {
+        for (int j = 0; j < t; j++)
+            z->C[j] = y->C[j + t];
+    }
+
+    y->n = t - 1;
+
+    for (int j = n; j >= i + 1; j--)
+        C[j + 1] = C[j];
+
+    C[i + 1] = z;
+
+    for (int j = n - 1; j >= i; j--)
+        keys[j + 1].key = keys[j].key;
+
+    keys[i].key = y->keys[t - 1].key;
+
+    n = n + 1;
+}
+
+void BTreeNode::traverse()
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+
+        if (leaf == false)
+            C[i]->traverse();
+        cout << "Record: " << i + 1 << endl;
+        cout << "First Name: " << keys[i].first_name << endl;
+        cout << "Last Name: " << keys[i].last_name << endl;
+        cout << "Course: " << keys[i].course << endl;
+        cout << "Hostel Name: " << keys[i].hostel_name << endl
+             << endl;
+        // cout << " " << keys[i].key;
+        addStudentToCSV(keys[i]);
+    }
+
+    // Print the subtree rooted with last child
+    if (leaf == false)
+        C[i]->traverse();
+}
+
+BTreeNode *BTree::searchh(int key)
+{
+    BTreeNode *node = root;
+
+    while (node != NULL)
+    {
+
+        int i = 0;
+        while (i < node->n && key > node->keys[i].key)
+        {
+            i++;
+        }
+
+        if (i < node->n && key == node->keys[i].key)
+        {
+            cout << "Record: " << i + 1 << endl;
+            cout << "First Name: " << node->keys[i].first_name << endl;
+            cout << "Last Name: " << node->keys[i].last_name << endl;
+            cout << "Course: " << node->keys[i].course << endl;
+            cout << "Hostel Name: " << node->keys[i].hostel_name << endl
+                 << endl;
+            return node;
+        }
+
+        else if (node->leaf)
+        {
+            cout << "\n\t\t\tData Not Found\n"
+                 << endl;
+            return NULL;
+        }
+
+        else
+        {
+            node = node->C[i];
+        }
+    }
+
+    cout << "\n\t\t\tData Not Found\n"
+         << endl;
+    return NULL;
+}
+
+BTreeNode *BTreeNode::search(student k)
+{
+
+    int i = 0;
+    while (i < n && k.key > keys[i].key)
+        i++;
+
+    if (keys[i].key == k.key)
+        return this;
+
+    if (leaf == true)
+        return NULL;
+
+    return C[i]->search(k);
+}
+
+void BTree::remove(student k)
+{
+    if (!root)
+    {
+        cout << "The tree is empty\n";
+        return;
+    }
+
+    root->remove(k);
+
+    if (root->n == 0)
+    {
+        BTreeNode *tmp = root;
+        if (root->leaf)
+            root = NULL;
+        else
+            root = root->C[0];
+
+        delete tmp;
+    }
+    return;
+}
+
+int main()
+{
+
+    Authentication auth;
+    bool isLoggedIn = false;
+
+    BTree t(3);
+    char another, choice;
+    struct student e;
+    char xfirst_name[50], xlast_name[50];
+    long int recsize;
+    recsize = sizeof(e);
+
+    while (1)
+    {
+        if (isLoggedIn == true or auth.login_status() == true)
+        {
+            isLoggedIn = true;
+            cout << "\t\t====== STUDENT DATABASE MANAGEMENT SYSTEM ======";
+            cout << "\n\n                                          ";
+            cout << "\n \t\t\t 0. Search Record";
+            cout << "\n \t\t\t 1. Add    Records";
+            cout << "\n \t\t\t 2. List Records";
+            cout << "\n \t\t\t 3. Modify Records";
+            cout << "\n \t\t\t 4. Delete Records";
+            cout << "\n \t\t\t 5. Exit   Program";
+            cout << "\n\n";
+            cout << "\t\t\t Select Your Choice :=> ";
+            // fflush(stdin);
+            // scanf("%c", choice);
+            cin >> choice;
+            switch (choice)
+            {
+            case '0':
+            {
+                cout << "Enter Roll number to search: ";
+                int r;
+                cin >> r;
+
+                t.searchh(r);
+            }
+            break;
+
+            case '1':
+            {
+                cout << "Enter the roll no : ";
+                cin >> e.key;
+                cout << "Enter the First Name : ";
+                cin >> e.first_name;
+                cout << "Enter the Last Name : ";
+                cin >> e.last_name;
+                cout << "Enter the Course    : ";
+                cin >> e.course;
+                cout << "Enter the Hostel Name   : ";
+                cin >> e.hostel_name;
+
+                t.insert(e);
+
+                if (!cin)
+                    cin.clear();
+                cin.ignore(80, '\n');
+            }
+            break;
+
+            case '2':
+            {
+                t.traverse();
+                if (!cin)
+                    cin.clear();
+                cin.ignore(80, '\n');
+            }
+            break;
+
+            case '3':
+            {
+                cout << "Enter the roll no: ";
+                cin >> e.key;
+                if (t.search(e) == NULL)
+                {
+                    cout << "Student's data with this roll no. is not present in the database" << endl;
+                }
+                else
+                {
+                    int key = e.key;
+                    t.remove(e);
+                    e.key = key;
+                    cout << "Enter the Firt Name : ";
+                    cin >> e.first_name;
+                    cout << "Enter the Last Name : ";
+                    cin >> e.last_name;
+                    cout << "Enter the Course    : ";
+                    cin >> e.course;
+                    cout << "Enter the Hostel Name   : ";
+                    cin >> e.hostel_name;
+                    t.insert(e);
+                }
+                if (!cin)
+                    cin.clear();
+                cin.ignore(80, '\n');
+            }
+            break;
+
+            case '4':
+            {
+                cout << "Enter the roll no: ";
+                cin >> e.key;
+                t.remove(e);
+                if (!cin)
+                    cin.clear();
+                cin.ignore(80, '\n');
+            }
+            break;
+
+            case '5':
+            {
+                cout << "\n\n";
+                cout << "\t\t";
+                cout << "\n\n";
+                exit(0);
+                if (!cin)
+                    cin.clear();
+                cin.ignore(80, '\n');
+            }
+            break;
+            }
+        }
+    }
+
+    return 0;
+}
